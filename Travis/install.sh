6f4a1a2703dd4007d270b91d10db11a2335550c6
root <root@xtest-3.tivlab.austin.ibm.com>
2016-11-10 16:24:12 -0600

    updating install

--- Travis/install.sh
+++ Travis/install.sh
@@ -4,5 +4,6 @@ echo 'Installing qiBuild!!!!'
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 #brew install cmake
 pip install qibuild
-qitoolchain info
+cd ../self-sdk/
+./scripts/build_mac.sh mac
 
